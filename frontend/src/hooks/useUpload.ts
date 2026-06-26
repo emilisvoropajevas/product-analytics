83b4649e81323c94afe7588cfad12609aa4b0bcf
Emilis Voropajevas <emilisvoropajevas@gmail.com>
2026-06-26 16:06:05 +0100

    Added useUpload mutation for uploading files with error handling

---
+++ frontend/src/hooks/useUpload.ts
@@ -0,0 +1,53 @@
+import { useMutation } from "@tanstack/react-query";
+import type { AxiosError } from "axios";
+import { toast } from 'sonner'
+
+import {
+    type BodyUploadOrders as UploadData,
+    Upload,
+} from "../client-axios";
+import { extractErrorMessage } from "../utils";
+
+interface UploadStatus {
+    chunk: number
+    status: "success" | "failed"
+    inserted_rows: number
+    reason: string | null
+    row_start: number
+    row_end: number
+}
+
+interface UploadStatusResponse {
+    total_successful: number
+    total_failed: number
+    data: Array<UploadStatus>
+}
+
+const useUpload = () => {
+    const upload = async (data : UploadData) => {
+        const response = await Upload.uploadOrders({
+            body: data,
+        })
+        return response
+    }
+
+    const uploadMutation = useMutation({
+        mutationFn: upload,
+        onSuccess: (response) => {
+            const result = response.data as unknown as UploadStatusResponse
+            if (result.total_failed > 0) {
+                toast.warning(`Upload partial: ${result.total_failed} chunks failed`)
+            } else {
+                toast.success("Data Uploaded successfully")
+            }
+        },
+        onError: (err) => {
+            toast.error(extractErrorMessage(err as AxiosError))
+        }
+    })
+    return {
+        uploadMutation
+    }
+}
+
+export default useUpload
--- frontend/src/routes/_layout/index.tsx
+++ frontend/src/routes/_layout/index.tsx
@@ -20,15 +20,21 @@ export const Route = createFileRoute("/_layout/")({
 
 
 function Dashboard() {
+    const [uploadPanel, showUploadPanel] = useState(false);
     return (
         <div className="bg-white border-b border-gray-200 px-6 py-8">
             <h1 className="text-4xl font-medium">Welcome Back, Admin</h1>
             <div className="flex gap-2 text-black rounded-full py-2">
-                <button className="bg-gray-900 text-white px-4 py-2 rounded-md text-sm hover:bg-gray-700">Upload CSV data</button>
+                <button type="button" onClick={() => showUploadPanel(prev => !prev)} className="bg-gray-900 text-white px-4 py-2 rounded-md text-sm hover:bg-gray-700">Upload CSV data</button>
+                {uploadPanel && <div>
+                    
+                    </div>}
                 <button className="border border-gray-300 text-gray-700 px-4 py-2 rounded-md text-sm">New Report</button>
             </div>
         </div>
     )
 }
 
-/* Finish on clicks for buttons, could render those in components */
+// Add full data summary table
+// Upload CSV -> Component that opens uploads data, shows successfull entries
+// add functionality to delete orders
